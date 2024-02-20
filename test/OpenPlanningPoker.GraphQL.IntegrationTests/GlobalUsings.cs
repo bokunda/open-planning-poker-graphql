8a8e0beac80fe2c096d7a01459daa96c0c769b77
Bojan Piskulić <bpiskulic@qubitsoft.rs>
2024-02-20 02:22:45 +0100

    Support for unit and integration tests

--- test/OpenPlanningPoker.GraphQL.IntegrationTests/GlobalUsings.cs
+++ test/OpenPlanningPoker.GraphQL.IntegrationTests/GlobalUsings.cs
@@ -1,1 +1,6 @@
+global using HotChocolate;
+global using HotChocolate.Execution;
+global using Microsoft.Extensions.DependencyInjection;
+global using OpenPlanningPoker.GraphQL.Api.Extensions;
+global using Snapshooter.Xunit;
 global using Xunit;
---
+++ test/OpenPlanningPoker.GraphQL.IntegrationTests/Schema/SchemaTests.cs
@@ -0,0 +1,19 @@
+﻿namespace OpenPlanningPoker.GraphQL.IntegrationTests.Schema;
+
+public class SchemaTests
+{
+    [Fact]
+    public async Task SchemaChangeTest()
+    {
+        // Arrange
+        var schema = await new ServiceCollection()
+            .AddGraphQlWithSchema()
+            .BuildSchemaAsync();
+
+        // Act
+        var schemaAsString = schema.ToString();
+
+        // Assert
+        schemaAsString.MatchSnapshot();
+    }
+}
